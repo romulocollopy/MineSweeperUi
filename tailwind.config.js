eea75501d66076ae3a922149781d5510d0d5ad06
Romulo Collopy <rcollopy@simplefractal.com>
2025-12-05 18:40:27 -0500

    customise font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Indie Flower', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
